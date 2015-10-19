bc59ba9a10c9f8feefd76cfe9ac6eb1043c7d9a2
johnv <john.voskuilen@sapito.nl>
2015-10-19 18:15:06 +0200

    Issue #2351299: Undo Prepare D8-Port: split class files

--- includes/Entity/WorkflowState.php
+++ includes/Entity/WorkflowState.php
@@ -3,6 +3,7 @@
 /**
  * @file
  * Contains workflow\includes\Entity\WorkflowState.
+ * Contains workflow\includes\Entity\WorkflowStateController.
  */
 
 class WorkflowState extends Entity {
@@ -491,6 +492,49 @@ class WorkflowState extends Entity {
   }
   public function value() {
     return $this->sid;
+  }
+
+}
+
+class WorkflowStateController extends EntityAPIController {
+
+  public function save($entity, DatabaseTransaction $transaction = NULL) {
+    // Create the machine_name.
+    if (empty($entity->name)) {
+      if ($label = $entity->state) {
+        $entity->name = str_replace(' ', '_', strtolower($label));
+      }
+      else {
+        $entity->name = 'state_' . $entity->sid;
+      }
+    }
+
+    $return = parent::save($entity, $transaction);
+    if ($return) {
+      $workflow = $entity->getWorkflow();
+      // Maintain the new object in the workflow.
+      $workflow->states[$entity->sid] = $entity;
+    }
+
+    // Reset the cache for the affected workflow.
+    workflow_reset_cache($entity->wid);
+
+    return $return;
+  }
+
+  public function delete($ids, DatabaseTransaction $transaction = NULL) {
+    // @todo: replace with parent.
+    foreach ($ids as $id) {
+      if ($state = workflow_state_load($id)) {
+        $wid = $state->wid;
+        db_delete('workflow_states')
+          ->condition('sid', $state->sid)
+          ->execute();
+
+        // Reset the cache for the affected workflow.
+        workflow_reset_cache($wid);
+      }
+    }
   }
 
 }
